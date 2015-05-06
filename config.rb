27a07952bda228bffd216b2b9bef18b894edf63e
Fernando Val <aaromnido@gmail.com>
2015-05-06 20:12:49 +0200

    build and config.rb changes

--- config.rb
+++ config.rb
@@ -73,6 +73,7 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.remote   = 'aaromnido'
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
