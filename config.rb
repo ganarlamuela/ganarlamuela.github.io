7723953f7ac8022f2ed23bb864a7ad8668835526
Fernando Val <aaromnido@gmail.com>
2015-05-06 20:22:01 +0200

    test

--- config.rb
+++ config.rb
@@ -33,7 +33,7 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -65,7 +65,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
+  # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
