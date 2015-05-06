bb065eaa60b6d665f72866bb887692171a576749
Fernando Val <aaromnido@gmail.com>
2015-05-06 23:30:25 +0200

    Programa page ingluded

---
+++ build/javascripts/scripts.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function(){
+    $("body").css("border", "10px solid red");
+    //$('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+});
---
+++ source/javascripts/scripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    //$('.aside-nav .row').pushpin({ top: $('.aside-nav').offset().top });
+});
